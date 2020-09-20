e29b1c13eb697ba29917d8cef20abec7076b8fd3
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2020-09-21 01:08:17 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -3,7 +3,7 @@ int main()
 {
     int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;   
     printf("Enter total number of processes:");
-    scanf("%d",&n);
+    scanf("%d",&n);    
     printf("Enter  Burst Time");
     for(i=0;i<n;i++)
     {
